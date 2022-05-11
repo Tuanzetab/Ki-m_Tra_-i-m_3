0ce917eb1b9e22dfc75cdd2643b57549bf586994
Hồ Anh Tuấn <97600240+Tuanzetab@users.noreply.github.com>
2022-05-11 17:47:46 +0700

    Update NoTaxProduct.java

--- NoTaxProduct.java
+++ NoTaxProduct.java
@@ -1,5 +1,11 @@
 
 public class NoTaxProduct extends Product {
+	public NoTaxProduct(){        
+    }  
+    public NoTaxProduct(String name, double price){
+        super(name, price);
+    }
+	
    public double getImportTax() {	
 		  return 0;
 	  }
